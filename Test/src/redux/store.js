090a2f44751db5c6e23e1f3828adeed1087b9ba0
Mikhail <bumial@yandex.ru>
2021-10-29 13:48:06 +0500

    first commit

--- Test/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunk from "redux-thunk";
-import data from './reducers/data';
-
-const store = createStore(combineReducers({
-    data
-}), applyMiddleware(thunk))
-
-export default store
